75e5b87016268af3305bfe04985e222a022deb28
sillyangel <sillyangel3@gmail.com>
2022-05-14 18:49:15 -0500

    fj

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 const log = require('./modules/log.js');
-const Client = require
-log("WIP")
+
+class user {
+    constructor(user, email) {
+        this.user = user;
+        this.email = email;
+        return console.log("Added, " + user + " with the email, " + email);
+    }
+    getemail() {
+        return console.log(this.email);
+    }
+    getuser() {
+        return console.log(this.user);
+    }
+    changeEmail(newemail) {
+        this.email = newemail;
+        return console.log("Changed the email to, " + newemail);
+    }
+    changeusername(newusername) {
+        this.user = newusername;
+        return console.log("Changed the username to, " + newusername);
+    }
+}
